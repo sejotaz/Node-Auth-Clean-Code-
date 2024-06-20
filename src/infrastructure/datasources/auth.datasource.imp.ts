a4c8028a2d4e130edf9cbd8b67c04dcbf36f1a92
cc-alejandro-ospina <98005752+sejotaz@users.noreply.github.com>
2024-06-20 17:54:02 -0500

    Se hace implemenaciones
    Se hace las implementaciones de data source y repository en infrastructure

---
+++ src/infrastructure/datasources/auth.datasource.imp.ts
@@ -0,0 +1,34 @@
+import { AuthDatasource, CustomError, RegisterUserDto, UserEntity } from "../../domain";
+
+
+export class AuthDatasourceImpl implements AuthDatasource{
+  async register(registerUserDto: RegisterUserDto): Promise<UserEntity> {
+
+    const { name, email, password } = registerUserDto
+
+    try {
+
+      //1. Verificar si el correo existe
+
+      //2. Hash de contraseña
+
+      //3. Mapear la respuesta de nuestra entidad
+      return new UserEntity(
+        '1', 
+        name,
+        email,
+        password,
+        ['ADMIN_ROLE'],
+      )
+      
+    } catch (error) {
+      if(error instanceof CustomError){
+        throw error
+      }
+      throw CustomError.internalServer()
+    }
+
+
+  }
+
+}
---
+++ src/infrastructure/index.ts
@@ -0,0 +1,2 @@
+export * from './datasources/auth.datasource.imp'
+export * from './repositories/auth.repository.imp'
