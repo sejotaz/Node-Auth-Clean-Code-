d2999d53503c9fc7e75540f784a18001c078a08c
Sejotaz <alejospina08@gmail.com>
2024-07-09 21:55:19 -0500

    Consumir el repositorio directamente

--- src/presentation/auth/routes.ts
+++ src/presentation/auth/routes.ts
@@ -1,11 +1,15 @@
 import { Router } from "express";
 import { AuthController } from "./controller";
+import { AuthDatasourceImpl, AuthRepositoryImpl } from "../../infrastructure";
 
 export class AuthRoutes {
   static get routes(): Router{
 
     const router = Router()
-    const controller = new AuthController()
+
+    const datasource = new AuthDatasourceImpl()
+    const authRepository = new AuthRepositoryImpl(datasource)
+    const controller = new AuthController(authRepository)
 
     router.post('/login', controller.loginUser)
 
