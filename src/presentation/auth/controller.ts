2a173e1dff8a95170149ae7c735aea83a28e9e2d
cc-alejandro-ospina <98005752+sejotaz@users.noreply.github.com>
2024-07-24 17:08:10 -0500

    Consumnir el repository

--- src/presentation/auth/controller.ts
+++ src/presentation/auth/controller.ts
@@ -10,9 +10,11 @@ export class AuthController {
   registerUser = (req: Request, res: Response) => {
     const [error, registerUserDto] = RegisterUserDto.create(req.body)
     if (error) return res.status(400).json(error)
-    this.authRepository.register(registerUserDto!)
-      .then(user => res.json(user))
-      .catch(error => res.status(500).json(error))
+
+      this.authRepository.register(registerUserDto!)
+        .then( user => res.json(user))
+        .catch( error => res.status(500).json(error) )
+
   }
 
   loginUser = (req: Request, res: Response) => {
